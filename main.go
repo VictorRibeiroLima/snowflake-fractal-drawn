4e5c86617e88e417a74808272f32cb0c1f26a405
Victor Ribeiro de Lima <victorlima.12@hotmail.com>
2022-09-17 01:47:15 -0300

    refactor: remove some duplicated code

--- main.go
+++ main.go
@@ -12,36 +12,51 @@ import (
 	"fyne.io/fyne/v2/container"
 )
 
-var CIRCLE_SIZE = fyne.NewSize(5, 5)
+func createCircle(position fyne.Position) *canvas.Circle {
+	circle := canvas.NewCircle(color.Black)
+	circle.Move(position)
+	circle.Resize(fyne.NewSize(4, 4))
+	return circle
+}
 
 func initBaseCircles() [6]*canvas.Circle {
 	var circles [6]*canvas.Circle
-	circles[0] = canvas.NewCircle(color.Black)
-	circles[0].Move(fyne.NewPos(225, 100))
-	circles[0].Resize(CIRCLE_SIZE)
-
-	circles[1] = canvas.NewCircle(color.Black)
-	circles[1].Move(fyne.NewPos(525, 100))
-	circles[1].Resize(CIRCLE_SIZE)
-
-	circles[2] = canvas.NewCircle(color.Black)
-	circles[2].Move(fyne.NewPos(75, 350))
-	circles[2].Resize(CIRCLE_SIZE)
-
-	circles[3] = canvas.NewCircle(color.Black)
-	circles[3].Move(fyne.NewPos(675, 350))
-	circles[3].Resize(CIRCLE_SIZE)
-
-	circles[4] = canvas.NewCircle(color.Black)
-	circles[4].Move(fyne.NewPos(225, 600))
-	circles[4].Resize(CIRCLE_SIZE)
-
-	circles[5] = canvas.NewCircle(color.Black)
-	circles[5].Move(fyne.NewPos(525, 600))
-	circles[5].Resize(CIRCLE_SIZE)
+	circles[0] = createCircle(fyne.NewPos(225, 100))
+	circles[1] = createCircle(fyne.NewPos(525, 100))
+	circles[2] = createCircle(fyne.NewPos(75, 350))
+	circles[3] = createCircle(fyne.NewPos(675, 350))
+	circles[4] = createCircle(fyne.NewPos(225, 600))
+	circles[5] = createCircle(fyne.NewPos(525, 600))
 
 	return circles
+}
 
+func calculatePosition(initial float32, target float32) float32 {
+	if initial != target {
+		sub := math.Abs(float64(initial - target))
+		twoThirds := sub / 3 * 2
+		if initial > target {
+			return float32(float64(initial) - twoThirds)
+		} else {
+			return float32(float64(initial) + twoThirds)
+		}
+	}
+	return initial
+}
+
+func drawLoop(circles [6]*canvas.Circle, base *fyne.Container) {
+	x, y := circles[0].Position().X, circles[0].Position().Y
+	for {
+		poss := rand.Intn(6)
+		targetCircle := circles[poss]
+		targetX, targetY := targetCircle.Position().X, targetCircle.Position().Y
+		x = calculatePosition(x, targetX)
+		y = calculatePosition(y, targetY)
+		circle := createCircle(fyne.NewPos(x, y))
+		base.Add(circle)
+		base.Refresh()
+		time.Sleep(time.Microsecond * 500)
+	}
 }
 
 func main() {
@@ -51,7 +66,7 @@ func main() {
 
 	base := container.NewWithoutLayout()
 	for _, circle := range circles {
-		base.AddObject(circle)
+		base.Add(circle)
 	}
 
 	w.SetContent(base)
@@ -61,37 +76,4 @@ func main() {
 	go drawLoop(circles, base)
 
 	w.ShowAndRun()
-}
-
-func drawLoop(circles [6]*canvas.Circle, base *fyne.Container) {
-	x, y := circles[0].Position().X, circles[0].Position().Y
-	for {
-		poss := rand.Intn(6)
-		targetCircle := circles[poss]
-		targetX, targetY := targetCircle.Position().X, targetCircle.Position().Y
-		if x != targetX {
-			sub := math.Abs(float64(x - targetX))
-			twoThirds := sub / 3 * 2
-			if x > targetX {
-				x = float32(math.Abs(float64(x) - twoThirds))
-			} else {
-				x = float32(float64(x) + twoThirds)
-			}
-		}
-		if y != targetY {
-			sub := math.Abs(float64(y - targetY))
-			twoThirds := sub / 3 * 2
-			if y > targetY {
-				y = float32(math.Abs(float64(y) - twoThirds))
-			} else {
-				y = float32(float64(y) + twoThirds)
-			}
-		}
-		circle := canvas.NewCircle(color.Black)
-		circle.Move(fyne.NewPos(x, y))
-		circle.Resize(CIRCLE_SIZE)
-		base.AddObject(circle)
-		base.Refresh()
-		time.Sleep(time.Microsecond * 500)
-	}
 }
